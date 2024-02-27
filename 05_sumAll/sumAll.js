3a947d66d89e840bd4b2e3b046b0883b0779d05c
SimonTPD <simon.tp.duthoit@gmail.com>
2024-02-26 17:07:34 -0800

    Submit exercise 05 solution
    Modified ".js" file with solution.
    Modified ".test.js" to remove all ".skip" so the ".js" file
    is fully tested.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if ((typeof num1 != 'number') || 
+        (typeof num2 != 'number') ||
+        (num1 < 0) || 
+        (num2 < 0)) return 'ERROR';
 
+    let numSum = Math.min(num1, num2); 
+    for(let i = Math.min(num1, num2) + 1; i <= Math.max(num1, num2); i++) numSum += i;
+
+    return numSum;
 };
 
 // Do not edit below this line
