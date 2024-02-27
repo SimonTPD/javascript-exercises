0295ce2b6ae6c062fdb583d3b50a3e841114bd80
SimonTPD <simon.tp.duthoit@gmail.com>
2024-02-26 16:33:54 -0800

    Submit JS exercise 03 solution
    Modified .js file with solution.
    Modified .text.js file to remove all ".skip" to fully test
    .js file

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(inputString) {
+    let outputString = '';
+    for (let i = 0; i < inputString.length; i++){
+         outputString = outputString.concat(inputString[inputString.length - i - 1]);
+    }
+    return outputString;
 };
 
 // Do not edit below this line
