5e0c54ae56f7545e7f3e9723f0d2e7bd2cff9bba
SimonTPD <simon.tp.duthoit@gmail.com>
2024-02-26 16:21:47 -0800

    Submit solution for exercise 02
    Modified .js file with the solution to the exercise.
    Modified .test.js file to remove the ".skip" from
    all tests so the .js file could be fully tested.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
+const repeatString = function(string, numRepeat) {
+    let returnString = '';
+    
+    if (numRepeat < 0) returnString = 'ERROR';
+    else for (let i = 0; i < numRepeat; i++) returnString += string;
 
+    return returnString;
 };
 
 // Do not edit below this line
