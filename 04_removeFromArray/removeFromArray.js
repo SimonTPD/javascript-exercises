fec7cd1f373600d1f35af2dd7267530f70922744
SimonTPD <simon.tp.duthoit@gmail.com>
2024-02-26 16:53:46 -0800

    Submit exercise 04 solution
    Modified ".js" file with solution.
    Modified ".test.js" to remove all ".skip" so the ".js" file
    is fully tested.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(inputArray) {
+    let returnArray = inputArray;
+    let elToRemove;
+    
+    for(let i = 1; i < arguments.length; i++){
+        elToRemove = arguments[i];
+        returnArray = returnArray.filter(el => el !== elToRemove);
+    }
+    
+    return returnArray;
 };
 
 // Do not edit below this line
