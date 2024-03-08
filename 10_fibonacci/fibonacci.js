d07b05090a44db5381c13040a399f034c9dce8fb
SimonTPD <simon.tp.duthoit@gmail.com>
2024-03-07 20:16:22 -0800

    Submit solution to exercise 10
    modified js file to solve exercise.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (+num < 0) return 'OOPS';
+    if (+num == 0) return 0;
+    if (+num <= 2) return 1;
+    let fib = [1, 1];
+    for (let i = 3; i <= num; i++){
+        fib = [fib[1], fib[0] + fib[1]];
+    }
+    return fib[1];
 };
 
 // Do not edit below this line
