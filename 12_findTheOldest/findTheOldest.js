30cc041cbf66f831bf98e1c23e7cb5a9d7997193
SimonTPD <simon.tp.duthoit@gmail.com>
2024-03-07 20:42:17 -0800

    Submit solution to exercise 12
    Modified js file to solve exercise.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(persons) {
+    return persons.map(el => {
+            const currentYear = new Date().getFullYear();
+            let age;
+            if (!('yearOfDeath' in el)) age = currentYear - el.yearOfBirth;
+            else age = el.yearOfDeath - el.yearOfBirth;
+            return {name: el.name, age: age};
+        })
+        .reduce((acc, el) => {
+            acc = (el.age > acc.age) ? el : acc;
+            return acc;
+        }, {name: 'None', age: 0});
 };
 
 // Do not edit below this line
