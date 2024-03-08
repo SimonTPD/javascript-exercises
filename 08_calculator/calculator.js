94e49ee97d1efe6da83208976c56b5dd2a9c3c68
SimonTPD <simon.tp.duthoit@gmail.com>
2024-03-07 18:59:42 -0800

    Submit solution to exercise 08
    Modified js file to solve exercise.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  if(arr.length == 0) return 0;
+  let sumArr = 0;
+  for (let i = 0; i < arr.length; i++) sumArr += arr[i];
+  return sumArr;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  if(arr.length == 0) return 0;
+  let prodArr = arr[0];
+  for (let i = 1; i < arr.length; i++) prodArr *= arr[i];
+  return prodArr;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num < 2) return 1;
+  let fact = 1;
+  for(let i = 2; i <= num; i++) fact *= i;
+  return fact;
 };
 
 // Do not edit below this line
