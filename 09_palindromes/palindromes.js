c8103166bb93257662cccc6f1adcf651bf7f2299
SimonTPD <simon.tp.duthoit@gmail.com>
2024-03-07 19:14:45 -0800

    Submit solution to exercise 09
    Modified js file to solve exercise.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,24 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    strToLC = str.toLowerCase();
+    let lettersAndDigitsOnly = [];
+    for(let i = 0; i < strToLC.length; i++){
+        let char = strToLC.charCodeAt(i);
+        if ((char >= 48 && char <= 57) ||
+            (char >= 65 && char <= 90) ||
+            (char >= 97 && char <= 122)){
+            lettersAndDigitsOnly.push(strToLC[i]);
+        }
+    }
+    strToCheck = lettersAndDigitsOnly.join('');
 
+    let isPal = true;
+    let i = 0;
+    while (i < Math.floor(strToCheck.length / 2) && isPal){
+        isPal = (strToCheck[i] == strToCheck[strToCheck.length - i - 1]) ? 
+            true : false;
+        i++;
+    }
+    return isPal;
 };
 
 // Do not edit below this line
